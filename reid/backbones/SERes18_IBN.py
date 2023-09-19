71b64f995ecbc86b3bec160374ed012ccb5245b2
Hu Mingzhe <hmz_superb@qq.com>
2023-09-19 16:06:06 -0600

    -1 cam factor

--- reid/backbones/SERes18_IBN.py
+++ reid/backbones/SERes18_IBN.py
@@ -248,7 +248,7 @@ class SERse18_IBN(nn.Module):
         self.is_reid = is_reid
         self.cam_bias = nn.Parameter(torch.randn(num_cams, 512))
         trunc_normal_(self.cam_bias, std=0.02)
-        self.cam_factor = 1.
+        self.cam_factor = -1.
 
     def forward(self, x, cam=None):
         x = self.conv0(x)
